5fcf8e023082406d8dfcb81fd289bcc62e662434
DimaGrishin <amvlastik@gmail.com>
2022-12-27 18:52:33 +0300

    Выполнено решение задачи

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] mass = { "Wor", "Google", "221", "Lololo" };
+
+string[] array = NewArray(mass);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+string[] NewArray(string[] array)
+{
+    int leng = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) leng++;
+    }
+    string[] result = new string[leng];
+    int k = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j].Length <= 3)
+        {
+            result[k] = array[j];
+            k++;
+        }
+    }
+    return result;
+}
